787190e006c0d4be982a579134c6754960c9e25f
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-06 20:08:12 -0500

    commit tree 17318db89d94b269413e551b096326c43a42b5c0

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+    def name
+        @name
+    end
+
+    def name=(nname)
+        @name = nname
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(nbreed)
+        @breed = nbreed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name
+        @name
+    end
+
+    def name=(nname)
+        @name = nname
+    end
+
+    def job
+        @job
+    end
+
+    def job=(njob)
+        @job = njob
+    end
+end
